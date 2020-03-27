30fb53e1487c163f21463aec8b7afd7b41ec6a67
Joao Santos <2190274@my.ipleiria.pt>
2020-03-27 19:12:42 +0000

    updated workflow

--- testing/test-pages.js
+++ testing/test-pages.js
@@ -2,28 +2,28 @@ var expect  = require('chai').expect;
 var request = require('request');
 
 it('Main page status', function(done) {
-    request('http://localhost:10000' , function(error, response, body) {
+    request('http://localhost:8080' , function(error, response, body) {
         expect(response.statusCode).to.equal(200);
         done();
     });
 });
 
 it('Search page status', function(done) {
-    request('http://localhost:10000/search.html' , function(error, response, body) {
+    request('http://localhost:8080/search.html' , function(error, response, body) {
         expect(response.statusCode).to.equal(200);
         done();
     });
 });
 
 it('History page status', function(done) {
-    request('http://localhost:10000/history.html' , function(error, response, body) {
+    request('http://localhost:8080/history.html' , function(error, response, body) {
         expect(response.statusCode).to.equal(200);
         done();
     });
 });
 
 it('Data Dashboard page status', function(done) {
-    request('http://localhost:10000/dashboard.html' , function(error, response, body) {
+    request('http://localhost:8080/dashboard.html' , function(error, response, body) {
         expect(response.statusCode).to.equal(200);
         done();
     });
